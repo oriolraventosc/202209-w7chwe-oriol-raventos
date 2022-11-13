a20f96b129409f66fae8bfb7183cd1994e670681
oriolraventosc <oriol.raventos.c@gmail.com>
2022-11-13 06:19:08 +0100

    Add the test of getUsers endpoint

---
+++ src/mocks/mockUsersList.ts
@@ -0,0 +1,79 @@
+const mockUsersList = [
+  {
+    username: "Pepe",
+    email: "pepe@gmail.com",
+    password: "$2a$10$eYBrhv0rpxo3Aai/9dQSPuimlzGTGM7PEKmB/Q3YzsgFKuA/iD0Sa",
+    image:
+      "https://images.unsplash.com/photo-1535931737580-a99567967ddc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    id: "63701d9c8699e8dc0848d978",
+  },
+  {
+    username: "Brenda",
+    email: "brenda@gmail.com",
+    password: "$2a$10$yok1cnr0LyjROXvDl0PFSeYShTfgLeAUoK9wYRaAvfb40goxHFf/2",
+    image:
+      "https://images.unsplash.com/photo-1485290334039-a3c69043e517?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    id: "63703d3259b78931cf9bc5da",
+  },
+  {
+    username: "Mike",
+    email: "mike@gmail.com",
+    password: "$2a$10$OaZGFq5/gfVA6MeOnHelTOKnENwXD/SgjU52nrFdjRHBH2b56anb.",
+    image:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    id: "63703d6159b78931cf9bc5dc",
+  },
+  {
+    username: "Yolanda",
+    email: "yolanda@gmail.com",
+    password: "$2a$10$APUMTq8oHTyIhBG6uVtVKOoSDn8z2Yq4fvsTVfta8bGTuhgtWJqfe",
+    image:
+      "https://images.unsplash.com/photo-1610766920689-5d4c57e1dfba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=564&q=80",
+    id: "63703d7859b78931cf9bc5de",
+  },
+  {
+    username: "Michael",
+    email: "michael@gmail.com",
+    password: "$2a$10$ApB8je9q9rw0eC5vJOH3g.bAOznPKpNCgDkkXN9qlQn9mpnd3GEDK",
+    image:
+      "https://images.unsplash.com/photo-1529903543134-d2d0b6858e21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    id: "63703db859b78931cf9bc5e0",
+  },
+  {
+    username: "Cindrel",
+    email: "cindrel@gmail.com",
+    password: "$2a$10$VbaQUm2/ePgcbNYzwjTFi.luMk2hmUAJPWka2wIFbwTC9lnU7v9ui",
+    image:
+      "https://images.unsplash.com/photo-1592992584424-bb6c75136025?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    id: "63703de559b78931cf9bc5e2",
+  },
+  {
+    username: "oriol",
+    email: "oriol@gmail.com",
+    password: "$2a$10$3UAZIt15qIznTCRNSxyUJOhaKKyLrwqjZXYFyCXQ3BNKubP12bnbq",
+    id: "63707240dcc23d471455c5c6",
+  },
+  {
+    username: "johny",
+    email: "johny@gmail.com",
+    password: "$2a$10$vVSQi49rX.WrxVEZhCjb9.ja.sUYNrqVbw8vEvqfww2HfZIomXQhm",
+    image: "johny.jpg",
+    id: "6370750f370e8eb1eb9360c4",
+  },
+  {
+    username: "Mars",
+    email: "mars@gmail.com",
+    password: "$2a$10$IuBT7qTv97HYeqwJChMP2OOEFv37NtSCCr2FJ.S5h/3PDx1zEQmQm",
+    image: "mars.jpg",
+    id: "637077db59b78931cf9c2f2b",
+  },
+  {
+    username: "jonathan",
+    email: "jonathan@gmail.com",
+    password: "$2a$10$YseV6GYHcIZF6Bif8hiAY.c3q2iu.whAlTBzX/ELWlMxew.YsBowi",
+    image: "jonathan.jpg",
+    id: "63707c416444c7304055c032",
+  },
+];
+
+export default mockUsersList;
